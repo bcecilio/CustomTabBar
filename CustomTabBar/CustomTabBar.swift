fc33727f2982327bc736fd1e9a11693dd317742e
brendon cecilio <brendon.cecilio@gmail.com>
2020-10-13 18:45:00 -0400

    added custom tabbar class

---
+++ CustomTabBar/CustomTabBar.swift
@@ -0,0 +1,53 @@
+//
+//  CustomTabBar.swift
+//  CustomTabBar
+//
+//  Created by Brendon Cecilio on 10/13/20.
+//
+
+import UIKit
+
+@IBDesignable class CustomTabBar: UITabBar {
+
+    private var shapeLayer: CALayer?
+    
+    override func draw(_ rect: CGRect) {
+        self.addShape()
+    }
+
+    private func addShape() {
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = createPath()
+        shapeLayer.strokeColor = UIColor.systemGray.cgColor
+        shapeLayer.fillColor = UIColor.white.cgColor
+        shapeLayer.lineWidth = 1.0
+        
+        if let oldShapeLayer = self.shapeLayer {
+            self.layer.replaceSublayer(oldShapeLayer, with: shapeLayer)
+        } else {
+            self.layer.insertSublayer(shapeLayer, at: 0)
+        }
+        self.shapeLayer = shapeLayer
+    }
+    
+    func createPath() -> CGPath {
+        
+        let height: CGFloat = 37.0
+        let path = UIBezierPath()
+        let centerWidth = self.frame.width
+        
+        path.move(to: CGPoint(x: 0, y: 0)) // start top left of frame
+        path.addLine(to: CGPoint(x: centerWidth - height * 2, y: 0))
+        // first curve down
+        path.addCurve(to: CGPoint(x: centerWidth, y: height), controlPoint1: CGPoint(x: centerWidth - 30, y: 0), controlPoint2: CGPoint(x: centerWidth + 30, y: 0))
+        // second curve up
+        path.addCurve(to: CGPoint(x: centerWidth + height * 2, y: height), controlPoint1: CGPoint(x: centerWidth + 35, y: height), controlPoint2: CGPoint(x: centerWidth + 30, y: 0))
+        
+        // complete rect
+        path.addLine(to: CGPoint(x: self.frame.width, y: 0))
+        path.addLine(to: CGPoint(x: self.frame.width, y: self.frame.width))
+        path.addLine(to: CGPoint(x: 0, y: self.frame.height))
+        
+        return path.cgPath
+    }
+}
